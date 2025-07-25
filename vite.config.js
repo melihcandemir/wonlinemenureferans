ff611a73dd51a29ecae437c59a13d1ce24bc4cf2
melihcandemir <melihcanndemir@gmail.com>
2025-07-26 01:04:36 +0300

    Fix deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base: "/wonlinemenureferans/", // GitHub repository adınız
+  plugins: [react()],
+  base: "/wonlinemenureferans/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
